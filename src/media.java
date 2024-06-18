256191f9e0623c035387e18c0efc27484471e3ae
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-06-18 15:52:23 -0300

    fiz o metodo x=

--- src/media.java
+++ src/media.java
@@ -5,11 +5,15 @@ public class media {
         float m = (n1 + n2)/2;
         System.out.println("A media é igual a : "+ m);*/
 
-        int numero = 10 ;
+        /*int numero = 10 ;
         int valor = 4 + numero--;
 
         System.out.println(valor);
-        System.out.println(numero);
+        System.out.println(numero);*/
+
+        /*int x = 4;
+        x *= 2; // é a mesma coisa que x = x *2
+        System.out.println(x);*/
 
     }
 }
