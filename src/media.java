55c885aecc29bcb17838e8e37c3a658c17111745
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-06-18 15:07:36 -0300

    coloquei o --

--- src/media.java
+++ src/media.java
@@ -1,9 +1,16 @@
 public class media {
     public static void main(String[] args) {
-        int n1 = 3;
+        /*int n1 = 3;
         int n2 = 5;
         float m = (n1 + n2)/2;
-        System.out.println("A media é igual a : "+ m);
+        System.out.println("A media é igual a : "+ m);*/
+
+        int numero = 10 ;
+        int valor = 4 + numero--;
+
+        System.out.println(valor);
+        System.out.println(numero);
+
     }
 }
 
