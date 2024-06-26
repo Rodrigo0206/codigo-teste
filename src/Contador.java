1f154eb13a448569e5102c8466b5be6c6e2b09fc
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-06-26 13:18:52 -0300

    adicionei o continue e o break

--- src/Contador.java
+++ src/Contador.java
@@ -1,9 +1,16 @@
 public class Contador {
     public static void main(String[] args) {
-        int cc = 1;
+        int cc = 0;
         while (cc<=20){ // é um contador utilizando o while ou "enquanto"
-            System.out.println("pulo " + cc);
+
             cc++;
+            if (cc == 2 || cc ==3 || cc == 4){
+                continue;
+            }
+            if (cc == 7){
+                break;
+            }
+            System.out.println("pulo " + cc);
         }
     }
 
