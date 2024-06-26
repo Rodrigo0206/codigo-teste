9d8e2b837b3984bd8b5989a847e62c63cec4c914
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-06-26 17:59:26 -0300

    fiz o metodo de repetição utilizando o do

---
+++ src/Repeticao.java
@@ -0,0 +1,9 @@
+public class Repeticao {
+    public static void main(String[] args) {
+        int cc = 0 ;                    //este metodo vai imprimir no console a string pula
+        do { // faça                    // enquanto a variavel cc for menor que 4
+            System.out.println("pula");
+            cc++;
+        }while (cc<4);
+    }
+}
