fca7efb68ad58226357f4676ab97a543869b3b33
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-06-28 17:01:03 -0300

    fiz um vetor simplificado

---
+++ src/Vetor02.java
@@ -0,0 +1,9 @@
+public class Vetor02 {
+    public static void main(String[] args) {
+
+        double v[] = {3.5, 2.75, 9, -4.5};// Este metodo imprimi no console
+        for (double valor: v){            // o conteudo das posições do vertor v
+            System.out.println(valor +" ");
+        }
+    }
+}
