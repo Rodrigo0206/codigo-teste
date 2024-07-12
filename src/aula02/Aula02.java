80f6e566a7ab1dd1d5adc9018fd8d82caa901fdc
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-12 10:28:52 -0300

    Alterei a visibilidade dos atributos e dos metodos da classe Caneta

--- src/aula02/Aula02.java
+++ src/aula02/Aula02.java
@@ -3,11 +3,11 @@ public class Aula02 {
     public static void main(String[] args) {
         Caneta c1 = new Caneta();
         c1.cor = "Azul";
-        c1.ponta = 0.5f;
+        //c1.ponta = 0.5f;
         c1.destampar();
         c1.rabiscar();
         c1.modelo = "bic";
-        c1.carga = 90;
+        //c1.carga = 90;
         c1.status();
     }
 }
--- src/aula02/Caneta.java
+++ src/aula02/Caneta.java
@@ -1,12 +1,12 @@
 package aula02;
 
 public class Caneta {
-    String modelo;
-    String cor;
-    float ponta;
-    int carga;
-    boolean tampada;
-    void status() {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    public void status() {
         System.out.println("Modelo: " + this.modelo);
         System.out.println("Uma caneta " + this.cor);
         System.out.println("Ponta: " + this.ponta);
@@ -24,11 +24,11 @@ public class Caneta {
 
     }
 
-    void tampar() {
+    public void tampar() {
         this.tampada = true;
     }
 
-    void destampar() {
+    public void destampar() {
         this.tampada = false;
     }
 }
