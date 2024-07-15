84a183d6b6c38d5ead9440e5e513b39cb0b5afb2
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-15 15:55:52 -0300

    Fiz os getters e setters e o contrutor

---
+++ src/aula04/Aula04.java
@@ -0,0 +1,10 @@
+package aula04;
+public class Aula04 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("bic", 1,"verde");
+        //c1.setModelo("BIC");
+        //c1.setPonta(0.5f);
+        //c1.status();
+        System.out.println();
+    }
+}
--- src/aula04/Caneta.java
+++ src/aula04/Caneta.java
@@ -1,21 +1,46 @@
 package aula04;
 public class Caneta {
-    public String modelo;
+    private String modelo;
     private float ponta;
+    private String cor;
+    private boolean tampada;
 
-    public String getModelo(){
-        return this.modelo;
+    public Caneta(String modelo, float ponta, String cor) {
+        this.modelo = modelo;
+        this.ponta = ponta;
+        this.cor = cor;
     }
 
-    public void setModelo(String m){
-        this.modelo = m;
+    public String getModelo() {
+        return modelo;
     }
 
-    public float getPonta(){
-        return this.ponta;
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
     }
 
-    public void setPonta(float p){
-        this.ponta = p;
+    public float getPonta() {
+        return ponta;
+    }
+
+    public void setPonta(float ponta) {
+        this.ponta = ponta;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public boolean isTampada() {
+        return tampada;
+    }
+
+    public void setTampada(boolean tampada) {
+        this.tampada = tampada;
     }
 }
+
