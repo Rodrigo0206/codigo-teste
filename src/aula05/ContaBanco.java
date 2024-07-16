86cea5d7a9e9b9c084d2abb2cb7c3c963dbe9614
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-16 14:44:43 -0300

    fiz os getters e setters

--- src/aula05/ContaBanco.java
+++ src/aula05/ContaBanco.java
@@ -22,4 +22,51 @@ public class ContaBanco {
     public void pagarMensal(){
 
     }
+    //metodos especiais
+
+
+    public ContaBanco() {
+        this.saldo = 0;
+        this.status = false;
+    }
+
+    public int getNumConta() {
+        return numConta;
+    }
+
+    public void setNumConta(int numConta) {
+        this.numConta = numConta;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public float getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
 }
