60b73d3bcbdfd91d0bf8daa2284546fa60a6b9c8
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-16 14:38:56 -0300

    fiz os atributos

---
+++ src/aula05/ContaBanco.java
@@ -0,0 +1,25 @@
+package aula05;
+public class ContaBanco {
+    //Atributos
+    public int numConta;
+    protected  String tipo;
+    private String dono;
+    private float saldo;
+    private boolean status;
+    //metodos personalizados
+    public void abrirConta(){
+
+    }
+    public void fecharConta(){
+
+    }
+    public void depositar(){
+
+    }
+    public void sacar(){
+
+    }
+    public void pagarMensal(){
+
+    }
+}
