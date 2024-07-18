c33c79f0106d34faeb7fa971c120ebb44f245d73
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-18 14:49:45 -0300

    fiz o controlador

---
+++ src/aula06/ControleRemoto.java
@@ -0,0 +1,15 @@
+package aula06;
+public class ControleRemoto {
+    //atributos
+    private int volume;
+    private boolean ligado;
+    private boolean tocando;
+    //Metodos especiais
+
+
+    public ControleRemoto() {
+        this.volume = 50;
+        this.ligado = false;
+        this.tocando = false;
+    }
+}
