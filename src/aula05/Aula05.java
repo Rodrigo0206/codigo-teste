e0f54da4cc9de733adad6304343f1352c62f316e
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-18 14:37:06 -0300

    fiz a interface

--- src/aula05/Aula05.java
+++ src/aula05/Aula05.java
@@ -1,11 +1,11 @@
 package aula05;
 public class Aula05 {
     public static void main(String[] args) {
-        ContaBanco p1 = new ContaBanco();
-        p1.setNumConta(1111);
-        p1.setDono("Rodrigo");
-        p1.abrirConta("CC");
-
+        ContaBanco p1 = new ContaBanco();       //Essa classe é um banco,
+        p1.setNumConta(1111);                   //que crio um usuario, posso
+        p1.setDono("Rodrigo");                  //colocar saldo, tirar saldo,
+        p1.abrirConta("CC");                  // abrir conta, fechar conta
+                                                //e no estadoAtual ele mostra o status
         ContaBanco p2 = new ContaBanco();
         p2.setNumConta(2222);
         p2.setDono("Ricardo");
