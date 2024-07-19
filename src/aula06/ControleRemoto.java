cbd3d828006ba589b2ab1acc697413bdd508cc71
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-19 09:06:44 -0300

    fiz os getters e setters

--- src/aula06/ControleRemoto.java
+++ src/aula06/ControleRemoto.java
@@ -12,4 +12,28 @@ public class ControleRemoto {
         this.ligado = false;
         this.tocando = false;
     }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
+    }
+
+    public boolean getLigado() {
+        return ligado;
+    }
+
+    public void setLigado(boolean ligado) {
+        this.ligado = ligado;
+    }
+
+    public boolean getTocando() {
+        return tocando;
+    }
+
+    public void setTocando(boolean tocando) {
+        this.tocando = tocando;
+    }
 }
