1705571bfb5fe89971fdbe0d8b9513eb3551e310
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-19 09:16:58 -0300

    eu implementei o controlador

--- src/aula06/ControleRemoto.java
+++ src/aula06/ControleRemoto.java
@@ -1,5 +1,5 @@
 package aula06;
-public class ControleRemoto {
+public class ControleRemoto implements Controlador {
     //atributos
     private int volume;
     private boolean ligado;
@@ -35,5 +35,55 @@ public class ControleRemoto {
 
     public void setTocando(boolean tocando) {
         this.tocando = tocando;
+    }
+
+    @Override
+    public void ligar() {
+
+    }
+
+    @Override
+    public void desligar() {
+
+    }
+
+    @Override
+    public void abrirMenu() {
+
+    }
+
+    @Override
+    public void fecharMenu() {
+
+    }
+
+    @Override
+    public void maisVolume() {
+
+    }
+
+    @Override
+    public void menosVolume() {
+
+    }
+
+    @Override
+    public void ligarMudo() {
+
+    }
+
+    @Override
+    public void desligarMudo() {
+
+    }
+
+    @Override
+    public void play() {
+
+    }
+
+    @Override
+    public void pause() {
+
     }
 }
