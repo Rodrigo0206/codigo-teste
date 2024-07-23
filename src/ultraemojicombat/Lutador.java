f9f50f229f7261d98945e8ab185c7bca14f6589d
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-23 15:53:04 -0300

    fiz os getters e setters

--- src/ultraemojicombat/Lutador.java
+++ src/ultraemojicombat/Lutador.java
@@ -21,5 +21,94 @@ public class Lutador {
     public void perderLuta(){
 
     }
+    public void empatarLuta(){
 
+    }
+    //Metodos especiais
+
+
+    public Lutador(String na, String no, int id, float al,
+                   float pe, int vi, int de, int em) {
+        this.nacionalidade = na;
+        this.nome = no;
+        this.idade = id;
+        this.altura = al;
+        this.peso = pe;
+        this.vitorias = vi;
+        this.derrotas = de;
+        this.empates = em;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getNacionalidade() {
+        return nacionalidade;
+    }
+
+    public void setNacionalidade(String nacionalidade) {
+        this.nacionalidade = nacionalidade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(float altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+        this.setCategoria();
+    }
+
+    public String getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria() {
+        this.categoria = categoria;
+    }
+
+    public int getVitorias() {
+        return vitorias;
+    }
+
+    public void setVitorias(int vitorias) {
+        this.vitorias = vitorias;
+    }
+
+    public int getDerrotas() {
+        return derrotas;
+    }
+
+    public void setDerrotas(int derrotas) {
+        this.derrotas = derrotas;
+    }
+
+    public int getEmpates() {
+        return empates;
+    }
+
+    public void setEmpates(int empates) {
+        this.empates = empates;
+    }
 }
