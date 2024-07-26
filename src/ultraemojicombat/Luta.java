00a122a91a9d18ee869c06a7efec4e7d9b089279
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-26 09:18:24 -0300

    fiz o metodo marcarLuta

--- src/ultraemojicombat/Luta.java
+++ src/ultraemojicombat/Luta.java
@@ -6,5 +6,55 @@ public class Luta {
     private Lutador desafiante;
     private int rounds;
     private boolean aprovada;
+    //Metodos publicos
+    public void marcarLuta(Lutador l1, Lutador l2){
+        if (l1.getCategoria().equals(l2.getCategoria())
+                && l1 != l2)  {
+            this.aprovada = true;
+            this.desafiado = l1;
+            this.desafiante = l2;
+        } else {
+            this.aprovada = false;
+            this.desafiado = null;
+            this.desafiante = null;
+        }
 
+    }
+    public void lutar(){
+
+    }
+    //Metodos especiais
+
+
+    public Lutador getDesafiado() {
+        return desafiado;
+    }
+
+    public void setDesafiado(Lutador desafiado) {
+        this.desafiado = desafiado;
+    }
+
+    public Lutador getDesafiante() {
+        return desafiante;
+    }
+
+    public void setDesafiante(Lutador desafiante) {
+        this.desafiante = desafiante;
+    }
+
+    public boolean isAprovada() {
+        return aprovada;
+    }
+
+    public void setAprovada(boolean aprovada) {
+        this.aprovada = aprovada;
+    }
+
+    public int getRounds() {
+        return rounds;
+    }
+
+    public void setRounds(int rounds) {
+        this.rounds = rounds;
+    }
 }
