56c4e44b29279cb082692345735f6c8a8ace2f6d
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-07-31 16:56:58 -0300

    fiz a classe principal

--- src/projetopessoas/ProjetoPessoas.java
+++ src/projetopessoas/ProjetoPessoas.java
@@ -2,6 +2,26 @@ package projetopessoas;
 
 public class ProjetoPessoas {
     public static void main(String[] args) {
+        //Programa Principal
+        Pessoa p1 = new Pessoa();
+        Aluno p2 = new Aluno();
+        Professor p3 = new Professor();
+        Funcionario p4 = new Funcionario();
+
+        p1.setNome("rodrigo");
+        p2.setNome("maria");
+        p3.setNome("gihh");
+        p4.setNome("ricardo");
+
+        p1.setSexo("M");
+        p4.setSexo("M");
+        p2.setIdade(18);
+
+        System.out.println(p1.toString());
+        System.out.println(p2.toString());
+        System.out.println(p3.toString());
+        System.out.println(p4.toString());
+
 
     }
 }
