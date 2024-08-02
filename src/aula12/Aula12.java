d3f9d660644e7cfe1d6715561b7d0c355abfc6f2
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-02 17:07:46 -0300

    fiz a classe principal

---
+++ src/aula12/Aula12.java
@@ -0,0 +1,36 @@
+package aula12;
+
+public class Aula12 {
+    public static void main(String[] args) {
+        //Animal n = new Animal() {
+       // }
+        Mamifero mamifero = new Mamifero();
+        Reptil reptil = new Reptil();
+        Peixe peixe = new Peixe();
+        Ave ave = new Ave();
+
+       /* mamifero.setPeso(35.5f);
+        mamifero.setCorPelo("Preto");
+        mamifero.alimentar();
+        mamifero.locomover();
+        mamifero.emitirSom();*/
+
+        Canguru canguru = new Canguru();
+        Cachorro cachorro = new Cachorro();
+        Cobra cobra = new Cobra();
+        Tartaruga tartaruga = new Tartaruga();
+        GoldFish goldFish = new GoldFish();
+        Arara arara = new Arara();
+
+        //.locomover();
+        cachorro.emitirSom();
+
+
+        //mamifero.locomover();
+        //reptil.locomover();
+        //peixe.locomover();
+
+
+
+    }
+}
