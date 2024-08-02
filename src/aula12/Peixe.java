99317268afb02fcb9afe394499e019937ea86bbe
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-02 16:28:36 -0300

    Fiz a classe Peixe

---
+++ src/aula12/Peixe.java
@@ -0,0 +1,24 @@
+package aula12;
+
+public class Peixe extends Animal{
+    private String corEscama;
+
+    @Override
+    public void locomover() {
+        System.out.println("Nadando");
+    }
+
+    @Override
+    public void alimentar() {
+        System.out.println("Comendo substancias");
+    }
+
+    @Override
+    public void emitirSom() {
+        System.out.println("glub glub");
+    }
+
+    public void soltarBolha(){
+        System.out.println("Soltou uma bolha");
+    }
+}
