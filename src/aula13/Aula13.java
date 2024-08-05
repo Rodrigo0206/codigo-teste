5323aa42c9fa14a27a330eae49ae6fe7197680ae
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-05 16:18:20 -0300

    fiz a classe Lobo e Cachorro

---
+++ src/aula13/Aula13.java
@@ -0,0 +1,10 @@
+package aula13;
+
+public class Aula13 {
+    public static void main(String[] args) {
+        // classe principal
+        Cachorro x = new Cachorro();
+        x.emitirSom();
+
+    }
+}
---
+++ src/aula13/Cachorro.java
@@ -0,0 +1,7 @@
+package aula13;
+public class Cachorro extends Lobo{
+    @Override
+    public void emitirSom(){
+        System.out.println("au..Au..au");
+    }
+}
