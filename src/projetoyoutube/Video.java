124486872395a2976ce2cae4c4f8bd7b69ae1a42
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-07 18:25:10 -0300

    Completei a classe gafanhoto

--- src/projetoyoutube/Video.java
+++ src/projetoyoutube/Video.java
@@ -56,16 +56,17 @@ public class Video implements AcoesVideo {
 
     @Override
     public void play() {
-
+        this.reproduzindo = true;
     }
 
     @Override
     public void pause() {
-
+        this.reproduzindo = false;
     }
 
     @Override
     public void like() {
+        this.curtidas ++;
 
     }
 
