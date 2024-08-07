3a67abfdeb5825bf92a4ee04eac08f035f25150b
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-08-07 16:37:06 -0300

    Fiz a classe Pessoa

--- src/projetoyoutube/ProjetoYouTube.java
+++ src/projetoyoutube/ProjetoYouTube.java
@@ -1,6 +1,12 @@
 package projetoyoutube;
 public class ProjetoYouTube {
     public static void main(String[] args) {
+        Video v[] = new Video[3];
+        v[0] = new Video("Aula 1 de poo");
+        v[1] = new Video("Aula 12 de PHP");
+        v[2] = new Video("aula 10 de HTML5");
+
+        System.out.println(v[0].toString());
 
     }
 }
