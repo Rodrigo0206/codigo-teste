181263c02f6ed467177411d33258894fd469d2a2
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-09-19 17:03:07 -0300

    fiz a classe Cliente

---
+++ src/lambda/Cliente.java
@@ -0,0 +1,12 @@
+package lambda;
+
+public class Cliente {
+
+    private String nome;
+    private Integer idade;
+
+    public Cliente(String nome, Integer idade){
+        this.nome = nome;
+        this.idade = idade;
+    }
+}
---
+++ src/lambda/Main.java
@@ -0,0 +1,16 @@
+package lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+
+        (argumento) -> expression
+                (argumento, argumento) -> expression
+                () -> {bloco}
+
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente(rodrigo))
+    }
+}
