9e2363cceac0ecc1f76254cb0582caeff71bfd48
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-09-19 17:08:06 -0300

    populei a arraylist  da classe main

--- src/lambda/Main.java
+++ src/lambda/Main.java
@@ -6,11 +6,19 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
 
-        (argumento) -> expression
-                (argumento, argumento) -> expression
-                () -> {bloco}
+       // (argumento) -> expression
+          //      (argumento, argumento) -> expression
+            //    () -> {bloco}
 
         List<Cliente> clientes = new ArrayList<>();
-        clientes.add(new Cliente(rodrigo))
+        clientes.add(new Cliente("rodrigo",37));
+        clientes.add(new Cliente("pedro",20));
+        clientes.add(new Cliente("carlos",40));
+        clientes.add(new Cliente("arthur",10));
+        clientes.add(new Cliente("francisco",20));
+        clientes.add(new Cliente("marcos",15));
+        clientes.add(new Cliente("paulo",70));
+        clientes.add(new Cliente("gustavo",18));
+
     }
 }
