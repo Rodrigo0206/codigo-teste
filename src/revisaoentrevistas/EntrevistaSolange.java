b09039715ce700e8cb5142fa27824de9b72d02ff
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-11-24 10:28:21 -0300

    usei a classe Scanner

--- src/revisaoentrevistas/EntrevistaSolange.java
+++ src/revisaoentrevistas/EntrevistaSolange.java
@@ -1,13 +1,20 @@
 package revisaoentrevistas;
 
+import java.util.Scanner;
+
 public class EntrevistaSolange {
     public static void main(String[] args) {
-        int x = 1;
+        /*int x = 1;
         int y = 2;
 
         int soma = x + y;
 
-        for ()
-        System.out.println(soma);
-    }
+        for (int a = 20; a > 0; a-- )
+        System.out.println(a);*/
+        //System.out.println("Digite um numero: ");
+        Scanner teclado = new Scanner(System.in);
+        String texto;
+        System.out.println("Entre com seu nome: ");
+        texto = teclado;
+
 }
