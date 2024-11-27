85c8c80ed240e54d5a9123b0992ba579ee4d51b4
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-11-27 10:43:48 -0300

    fiz oif e else da classe Aula04

--- src/rodrigofreitasjava/Aula04.java
+++ src/rodrigofreitasjava/Aula04.java
@@ -2,6 +2,12 @@ package rodrigofreitasjava;
 
 public class Aula04 {
     public static void main(String[] args) {
+        Integer a = 1;
+        if (a >= 18){
+            System.out.println("Maior de 18");
+        } else {
+            System.out.println("Menor de idade");
+        }
 
     }
 }
