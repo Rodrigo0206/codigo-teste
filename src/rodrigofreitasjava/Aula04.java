0580c4dc67a87a029204cc303b877d936bfe12b0
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-11-27 17:21:05 -0300

    fiz os ifs da idade

--- src/rodrigofreitasjava/Aula04.java
+++ src/rodrigofreitasjava/Aula04.java
@@ -1,13 +1,22 @@
 package rodrigofreitasjava;
 
+import java.util.Scanner;
+
 public class Aula04 {
     public static void main(String[] args) {
-        Integer a = 1;
-        if (a >= 18){
+
+       // Scanner scanner = new Scanner(System.in);
+        //Integer idade = Integer.valueOf(Integer.scanner);
+        Integer a = 150;
+        if (a >= 18 && a <=65){
             System.out.println("Maior de 18");
-        } else {
-            System.out.println("Menor de idade");
-        }
+        } else if (a <= 12) {
+            System.out.println(" Você é uma crianca ");
+        } else if (a < 18 && a > 12) {
+            System.out.println("Você é um adolescente ");
+        }else
+            System.out.println("idoso");
+
 
     }
 }
