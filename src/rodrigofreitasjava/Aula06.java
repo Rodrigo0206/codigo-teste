2e132cf50a3d067b59b235bf52caf108009b0e31
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-11-28 09:23:20 -0300

    Fiz o while

---
+++ src/rodrigofreitasjava/Aula06.java
@@ -0,0 +1,11 @@
+package rodrigofreitasjava;
+
+public class Aula06 {
+    public static void main(String[] args) {
+        Integer a = 0;
+        while (a < 5){
+            a = a +1;
+            System.out.println(a);
+        }
+    }
+}
--- src/rodrigofreitasjava/Funcionario.java
+++ src/rodrigofreitasjava/Funcionario.java
@@ -5,11 +5,11 @@ package rodrigofreitasjava;
 
 public class Funcionario {
     public static void main(String[] args) {
-        Pessoa Funcionario  = new Pessoa() {
+        //Pessoa Funcionario  = new Pessoa() {
         };
-        System.out.println(Funcionario.getNome());
+        //System.out.println(Funcionario.getNome());
     }
 
 
 
-}
+
