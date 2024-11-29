5949ea73d857cc9e47f7a54d1672948e0d79cfb3
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-11-29 10:53:35 -0300

    fiz o merge da lista

--- src/rodrigofreitasjava/Aula07ArrayMatrizVetor.java
+++ src/rodrigofreitasjava/Aula07ArrayMatrizVetor.java
@@ -6,14 +6,25 @@ import java.util.Scanner;
 public class Aula07ArrayMatrizVetor {
     public static void main(String[] args) {
        String [] lista = new String[3];
+       lista [0] = "rodrigo";
+       lista [1] = "ricardo";
+       lista [2] = "sol";
 
-        for (Integer  i = 0; i < 3 ; i ++){
+        System.out.println("O primeiro nome é: " + lista[0] + "\n"
+                + "O segundo nome é: " + lista[1] + "\n"
+                + "O terceiro nome é: " + lista[2]);
+
+
+
+
+
+       /* for (Integer  i = 0; i < 3 ; i ++){
             System.out.println("Digite o nome na posição : " + i);
             lista[i] = new Scanner(System.in).nextLine();
         }
         System.out.println("O primeiro nome digitado é: " + lista[0]);
         System.out.println("O segundo nome digitado é: " + lista[1]);
-        System.out.println("O terceiro nome digitado é: " + lista[2]);
+        System.out.println("O terceiro nome digitado é: " + lista[2]);*/
 
 
 
