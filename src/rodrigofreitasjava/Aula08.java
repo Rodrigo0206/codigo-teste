a892135fa9ce894c163a116f7305eda65d87971c
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-12-04 15:03:43 -0300

    fiz o metodo da aula08

---
+++ src/rodrigofreitasjava/Aula08.java
@@ -0,0 +1,33 @@
+package rodrigofreitasjava;
+
+public class Aula08 {
+    public static void main(String[] args) {
+
+        Integer a = 3;
+        Integer b = 2;
+        Integer soma = a +b;
+
+        if ( a % 2 ==0) {
+            soma = a +b;
+
+              }
+        else {
+            soma = a * b;
+        }
+        System.out.println(soma);
+    }
+    private static Integer calculo(Integer a, Integer b){
+        Integer soma = 0 ;
+
+        if ( a % 2 ==0) {
+            soma = a +b;
+
+        }
+        else {
+            soma = a * b;
+        }
+        System.out.println(soma);
+
+
+    }
+}
