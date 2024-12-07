2584d13eb804047d0d20f441e026395be31d9374
Rodrigo Domingues <91419413+Rodrigo0206@users.noreply.github.com>
2024-12-07 12:57:26 -0300

    fiz a classe calculo

--- src/rodrigofreitasjava/Aula09ClassesEObjetos.java
+++ src/rodrigofreitasjava/Aula09ClassesEObjetos.java
@@ -5,11 +5,14 @@ public class Aula09ClassesEObjetos {
     public static void main(String[] args) {
         Carro carro = new Carro();
         Carro carro2 = new Carro(3,5);
-        System.out.println(carro.getPortas() + "\n" + carro.getRodas());
+       // System.out.println(carro.getPortas() + "\n" + carro.getRodas());
         //carro.setPortas(1);
-        System.out.println(carro.getPortas());
-        System.out.println(carro2.getPortas());
-        System.out.println(carro2.getRodas());
+        //System.out.println(carro.getPortas());
+        //System.out.println(carro2.getPortas());
+       // System.out.println(carro2.getRodas());
+        System.out.println(
+                Calculo.soma(3,7)
+        );
 
     }
 }
---
+++ src/rodrigofreitasjava/Calculo.java
@@ -0,0 +1,8 @@
+package rodrigofreitasjava;
+
+public class Calculo {
+
+    public static Integer soma(Integer x, Integer y){
+        return x + y;
+    }
+}
